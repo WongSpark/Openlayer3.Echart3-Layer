7a42b412916834d83a2091ec81b4e45c8b15afa3
WongSpark <www.820767125@qq.com>
2017-08-11 20:55:37 +0800

    1

---
+++ dist/openlayers.e3.js
@@ -0,0 +1,363 @@
+
+var options = {
+    'container': 'front',
+    'renderer': 'dom',
+    'hideOnZooming': false,
+    'hideOnMoving': false,
+    'hideOnRotating': false
+};
+var ADLayer=function(id,ecOptions,map){
+	this._map=map;
+	this._ecOptions=ecOptions;
+	this._registered=false;
+	this._container=null;
+	this.zIndex=999;
+	this._options=options;
+	this._container=null;
+}
+ADLayer.prototype.getMap=function(){
+	return this._map;
+};
+ADLayer.prototype.show = function() {
+		if (this._container) {
+				this._container.style.display = '';
+		}
+};
+ADLayer.prototype._isVisible = function() {
+		return this._container && this._container.style.display === '';
+};
+ADLayer.prototype.render=function(){
+		//保证地图渲染完成才执行专题图渲染，且只执行一次
+    this._map.once('postrender',this._render,this);
+};
+ADLayer.prototype._render=function(){
+	if (!this._container) {
+			this._createLayerContainer();
+	}
+	if (!this._ec) {
+			this._ec = echarts.init(this._container);
+			this._prepareECharts();
+			this._ec.setOption(this._ecOptions, false);
+	} else if (this._isVisible()) {
+			this._ec.resize();
+	}
+	this._bindEvent();
+}
+ADLayer.prototype._bindEvent=function(){
+		this._map.on('precompose',this._moveHandler("onMoveEnd"),this)
+		//this._map.on('postrender',this._moveHandler("onMoveEnd"),this)
+		//this._map.on('postcompose',this._moveHandler("onMoveEnd"),this)
+    //this._map.on('moveend', this._moveHandler("onMoveEnd"),this);
+    //this._map.on('pointerdrag', this._moveHandler('onMoveEnd'),this);
+
+    //occured before the zoom event is triggered.
+    //this._map.getView().on('change:resolution', this._moveHandler("onZoomEnd"),this);
+
+    //window.addEventListener("resize", this._ec.resize, false);
+};
+ADLayer.prototype._moveHandler = function (type) {
+    var _this = this;
+
+    return _this[type];
+}
+ADLayer.prototype._prepareECharts = function() {
+	if (!this._registered) {
+			echarts.registerCoordinateSystem('maptalks', this._getE3CoordinateSystem(this.getMap()));
+			this._registered = true;
+	}
+	var series = this._ecOptions.series;
+	if (series) {
+			for (var i = series.length - 1; i >= 0; i--) {
+					series[i]['coordinateSystem'] = 'maptalks';
+
+					series[i]['animation'] = false;
+			}
+	}
+};
+
+ADLayer.prototype._createLayerContainer = function() {
+		var size=this._map.getSize();
+		var container = this._container = ADLayer.Element.createDiv('e_container',{
+			height:size[1]+'px',
+			width:size[0] + 'px',
+			top:'0px',
+			left:'0px',
+			zIndex:999
+		});
+		//container.style.cssText = 'position:absolute;left:0px;top:0px;';
+		if (this._zIndex) {
+				container.style.zIndex = this._zIndex;
+		}
+		this._resetContainer();
+		//ADLayer.Element.append(container);
+		this._map.getViewport().appendChild(this._container);
+};
+
+ADLayer.prototype._resetContainer = function() {
+		var size = this.getMap().getSize();
+
+		this._container.style.width = size.width + 'px';
+		this._container.style.height = size.height + 'px';
+};
+
+ADLayer.prototype._getE3CoordinateSystem = function(map) {
+		var CoordSystem = function CoordSystem(map) {
+				this.map = map;
+				this._mapOffset = [0, 0];
+		};
+
+		CoordSystem.create = function (ecModel) {
+				ecModel.eachSeries(function (seriesModel) {
+						if (seriesModel.get('coordinateSystem') === 'maptalks') {
+								seriesModel.coordinateSystem = new CoordSystem(map);
+						}
+				});
+		};
+
+		CoordSystem.getDimensionsInfo = function () {
+				return ['x', 'y'];
+		};
+
+		CoordSystem.dimensions = ['x', 'y'];
+
+		CoordSystem.prototype.dimensions= ['x', 'y'];
+		CoordSystem.prototype.setMapOffset=function setMapOffset(mapOffset) {
+				this._mapOffset = mapOffset;
+		};
+		CoordSystem.prototype.dataToPoint=function dataToPoint(data) {
+				//地理坐标转为屏幕坐标
+				//console.log(data);
+				//var coord = new maptalks.Coordinate(data);
+				var px = this.map.getPixelFromCoordinate(data);
+				var mapOffset = this._mapOffset;
+				return [px[0] - mapOffset[0], px[1] - mapOffset[1]];
+		},
+		CoordSystem.prototype.pointToData=function pointToData(pt) {
+				//屏幕坐标转为地理坐标
+				var mapOffset = this._mapOffset;
+				var data = this.map.containerPointToCoordinate({
+						x: pt[0] + mapOffset[0],
+						y: pt[1] + mapOffset[1]
+				});
+				return [data.x, data.y];
+		},
+		CoordSystem.prototype.getViewRect=function getViewRect() {
+				var size = this.map.getSize();
+				return new echarts.graphic.BoundingRect(0, 0, size.width, size.height);
+		},
+		CoordSystem.prototype.getRoamTransform=function getRoamTransform() {
+				return echarts.matrix.create();
+		}
+		return CoordSystem;
+};
+
+ADLayer.prototype.getEvents = function() {
+		return {
+				'_zoomstart': this.onZoomStart,
+				'_zoomend': this.onZoomEnd,
+				'_dragrotatestart': this.onDragRotateStart,
+				'_dragrotateend': this.onDragRotateEnd,
+				'_movestart': this.onMoveStart,
+				'_moveend': this.onMoveEnd,
+				'_resize': this._resetContainer
+		};
+};
+
+ADLayer.prototype._clearAndRedraw = function() {
+		if (this._container && this._container.style.display === 'none') {
+				return;
+		}
+		this._ec.clear();
+		this._ec.resize();
+		this._prepareECharts();
+		this._ec.setOption(this._ecOptions, false);
+};
+
+ADLayer.prototype.onZoomStart = function() {
+		// if (!this.options['hideOnZooming']) {
+		// 		return;
+		// }
+		this.hide();
+};
+
+ADLayer.prototype.onZoomEnd = function() {
+		// if (!this.options['hideOnZooming']) {
+		// 		return;
+		// }
+		//this.show();
+		this._clearAndRedraw();
+};
+
+ADLayer.prototype.onDragRotateStart = function() {
+		// if (!this.options['hideOnRotating']) {
+		// 		return;
+		// }
+		this.hide();
+};
+
+ADLayer.prototype.onDragRotateEnd = function() {
+		// if (!this.options['hideOnRotating']) {
+		// 		return;
+		// }
+		//this.show();
+		this._clearAndRedraw();
+};
+
+ADLayer.prototype.onMoveStart = function() {
+		// if (!this.options['hideOnMoving']) {
+		// 		return;
+		// }
+		this.hide();
+};
+
+ADLayer.prototype.show=function(){
+	if (this._container) {
+			this._container.style.display = '';
+	}
+}
+ADLayer.prototype.onMoveEnd = function() {
+		// if (!this.options['hideOnMoving']) {
+		// 		return;
+		// }
+		//this.show();
+
+		this._ec.resize();
+		this._ec.setOption(this._ecOptions, false);
+		// if (this._isVisible()){
+		// 	console.log(1);
+		// 	this._ec.resize();
+		// }
+};
+
+ADLayer.Element={
+	/**
+	 * 创建绝对定位div
+	 * @param id div id
+	 * @param style 样式的对象
+	 */
+	createDiv:function(id,style){
+		if(!id){
+			return console.error("id不能为空");
+		}
+		var div=document.createElement('div');
+		div.id=id;
+		div.style.position='absolute';
+		ADLayer.Element.modifyDomStyle(div,style);
+
+		return div;
+},
+	/**
+	 * 修改dom元素的样式相关属性
+	 * @param dom
+	 * @param style
+	 */
+	modifyDomStyle:function(dom,style){
+		if(!style){
+			return;
+		}
+		if(style.height){
+			dom.style.height=style.height;
+		}
+		if(style.width){
+			dom.style.width=style.width;
+		}
+		if(style.border){
+			dom.style.border=style.border;
+		}
+		if(style.transition){
+			dom.style.transition=style.transition;
+		}
+		if(style.color){
+			dom.style.color=style.color;
+		}
+		if(style.lineHeight){
+			dom.style.lineHeight=style.lineHeight;
+		}
+		if(style.borderRadius){
+			dom.style.borderRadius=style.borderRadius;
+		}
+		if(style.textAlign){
+			dom.style.textAlign=style.textAlign;
+		}
+		if(style.backgroundColor){
+			dom.style.backgroundColor=style.backgroundColor;
+		}
+		if(style.zIndex){
+			dom.style.zIndex=style.zIndex;
+		}
+		if(style.top){
+			dom.style.top=style.top;
+		}
+		if(style.left){
+			dom.style.left=style.left;
+		}
+	},
+	/**
+	 * 设置dom元素的绝对位置
+	 * @param dom 需要设定位置的dom元素
+	 * @param pos top，left数组,number
+	 */
+	setDomPosition:function(dom,pos){
+		dom.style.top=pos[0]+"px";
+		dom.style.left=pos[1]+"px";
+	},
+	/**
+	 * 设置dom的内容
+	 * @param inner 内容，可为文字，html
+	 */
+	setDomContent:function(dom,inner){
+		dom.innerHTML=inner;
+	},
+	/**
+	 * 在dom后追加元素，不指定id就在body后追加
+	 * @param dom
+	 * @param id
+	 * @returns
+	 */
+	append:function(dom,id){
+		if(typeof dom =="string"){
+			if(!id){
+				return document.body.innerHTML=dom;
+			}
+			var target=document.getElementById(id);
+			if(target){
+				return target.innerHTML=dom;
+			}
+		}else{
+			if(!id){
+				return document.body.appendChild(dom);
+			}
+			var target=document.getElementById(id);
+			if(target){
+				return target.appendChild(dom);
+			}
+		}
+	},
+	getDomStyle:function(dom,attr){
+		return dom.style[attr];
+	},
+	getDomCenter:function(dom,id){
+		var outer_h='100%',outer_w='100%',inner_h=0,inner_w=0;
+		if(!id){
+			outer_h=document.body.offsetHeight;
+			outer_w=document.body.offsetWidth;
+		}else{
+			var outer=document.getElementById(id);
+			if(outer){
+				outer_h=ADLayer.Element.getDomStyle(outer,'height');
+				outer_w=ADLayer.Element.getDomStyle(outer,'width');
+			}else{
+				return console.error("未能找到id为"+id+"的元素")
+			}
+		}
+
+		if(dom){
+			inner_h=ADLayer.Element.getDomStyle(dom,'height');
+			inner_w=ADLayer.Element.getDomStyle(dom,'width');
+		}else{
+			return console.error("未提供dom元素")
+		}
+
+		return [(parseInt(outer_h)-parseInt(inner_h))/2,(parseInt(outer_w)-parseInt(inner_w))/2]
+	},
+};
